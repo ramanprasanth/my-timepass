236b83951b320b4b13b1eec227f10b1858cf899c
unknown <raman.prasanth@gmail.com>
2020-05-07 05:35:00 +0530

    Adding logs folder

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask
 import webbrowser, logging
-
+import os, datetime
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.WARNING)
+current_directory = os.getcwd()
+if not os.path.isdir(current_directory+"/logs"):
+	os.mkdir(current_directory+"/logs")
+current_time = datetime.datetime.strftime(datetime.datetime.now(), "%d%m%Y%H%M%S")
+create_logfile = current_directory+"/logs/logs_"+current_time+".txt"
+logging.basicConfig(filename=create_logfile, level=20)
 
 @app.route('/', methods=["GET"])
 def home():
